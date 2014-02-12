d78cb2c1a6f380b840abe90122c53fa9b6e6e893
knap1930 <derek@kamcord.com>
2014-02-11 17:12:28 -0800

    fix ssl logic, should be working now

--- recipes/proxy.rb
+++ recipes/proxy.rb
@@ -8,28 +8,24 @@ template "#{node.elasticsearch[:nginx][:dir]}/conf.d/elasticsearch_proxy.conf" d
   notifies :reload, 'service[nginx]'
 end
 
-ruby_block "copy ssl certs" do
-  block do
-    # Create /etc/nginx/ssl directory on chef client
-    #
-    directory "#{node.elasticsearch[:nginx][:dir]}/ssl" do
-      action :create
-      recursive true
-      mode 0755
-    end
-
-    # Copy ssl certificates from certificates folder to client’s /etc/nginx/ssl folder
-    #
-    remote_directory "#{node.elasticsearch[:nginx][:dir]}/ssl" do
-      source "certificates"
-      files_owner "root"
-      files_group "root"
-      files_mode 00644
-      owner node.elasticsearch[:nginx][:user] and group node.elasticsearch[:nginx][:user] and mode 0755
-    end
+if node.elasticsearch[:nginx][:ssl]
+  # Create /etc/nginx/ssl directory on chef client
+  #
+  directory "#{node.elasticsearch[:nginx][:dir]}/ssl" do
+    action :create
+    recursive true
+    mode 0755
   end
 
-  not_if { node.elasticsearch[:nginx][:users].empty? }
+  # Copy ssl certificates from certificates folder to client’s /etc/nginx/ssl folder
+  #
+  remote_directory "#{node.elasticsearch[:nginx][:dir]}/ssl" do
+    source "certificates"
+    files_owner "root"
+    files_group "root"
+    files_mode 00644
+    owner node.elasticsearch[:nginx][:user] and group node.elasticsearch[:nginx][:user] and mode 0755
+  end
 end
 
 ruby_block "add users to passwords file" do
